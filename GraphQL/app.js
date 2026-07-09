bdde4cacf75041f0cbdd250883d46af4c8fa8d7d
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-07-09 15:51:44 +0530

    mutation and context in Graphql

--- GraphQL/app.js
+++ GraphQL/app.js
@@ -16,17 +16,38 @@ const typeDefs = `#graphql
     }
 
     type Query{
-        user:User,
+        user(id:ID!):User,
         users: [User!]!
+    }
+
+    input CreateUserInput{
+        name: String!
+        age: Int!
+        price:Int
+    }
+
+    type Mutation{
+        createUser(input:CreateUserInput!):User!
     }
 `;
 
 const resolvers = {
     Query: {
-        user: (_,args) => {
+        user: (_,args,context) => {
+            console.log(context);
             return users.find(user => user.id === args.id);
         },
         users: () =>  users
+    },
+    Mutation:{
+        createUser:(_,args) => {
+            const newUser = {
+                id:String(users.length+1),
+                ...args.input
+            }
+            users.push(newUser);
+            return newUser;
+        }
     }
 };
 
@@ -37,6 +58,12 @@ const server = new ApolloServer({
 
 const {url} = await startStandaloneServer(server,{
     listen:{port:8080},
+    context: async({req}) => {
+        return {
+            username:"yaksh",
+            role:"admin"
+        }
+    }
 })
 
 console.log(`Server running at ${url}`);
